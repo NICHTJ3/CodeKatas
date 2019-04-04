d3f396ddce3fd0cd3b99abee84dc840f8b8683f8
Trent Nicholson <nichtj3@student.op.ac.nz>
2019-04-05 00:38:45 +1300

    Prefer spread array
    Instead of fill in IfYouCantSleepJustCountSheep use spread array

--- Katas/IfYouCantSleepJustCountSheep.js
+++ Katas/IfYouCantSleepJustCountSheep.js
@@ -7,8 +7,8 @@ function countSheep(num) {
 }
 
 function countSheep(num) {
-  return Array(num).fill(0).reduce((total, _, currentIndex) => total+=`${currentIndex+1} sheep...`, "");
-  // return [...Array(num)].reduce((total, _, currentIndex) => total+=`${currentIndex+1} sheep...`, "");
+  //return Array(num).fill(0).reduce((total, _, currentIndex) => total+=`${currentIndex+1} sheep...`, "");
+  return [...Array(num)].reduce((total, _, currentIndex) => total+=`${currentIndex+1} sheep...`, "");
 }
 
 console.log(countSheep(1) === "1 sheep...");
