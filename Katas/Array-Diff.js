2b925e4fef629121ab38e2688078821ec6143448
Trent Nicholson <nichtj3@student.op.ac.nz>
2019-07-24 13:33:09 +1200

    Format with prettier

--- Katas/Array-Diff.js
+++ Katas/Array-Diff.js
@@ -1,23 +1,23 @@
 //Solutions
 
 //Filter array for things not in the other
-function array_diff(a,b){
-  return a.filter(c => !b.includes(c))
+function array_diff(a, b) {
+  return a.filter(c => !b.includes(c));
 }
 
 // First attempt does not work for random arrays
-function array_diff(a,b) {
-  var temp=[]
-  for (let [i,e] of a.entries()) {
-    if(b.indexOf(e)<0){
-      temp.push(e)
+function array_diff(a, b) {
+  var temp = [];
+  for (let [i, e] of a.entries()) {
+    if (b.indexOf(e) < 0) {
+      temp.push(e);
     }
   }
-  return temp
+  return temp;
 }
 
 // Tests
-console.log(array_diff([], [4,5]), []);
-console.log(array_diff([1,2,2], [2]), [1]);
-console.log(array_diff([3,4], [3]), [4]);
-console.log(array_diff([1,8,2], []), [1,8,2]);
+console.log(array_diff([], [4, 5]), []);
+console.log(array_diff([1, 2, 2], [2]), [1]);
+console.log(array_diff([3, 4], [3]), [4]);
+console.log(array_diff([1, 8, 2], []), [1, 8, 2]);
